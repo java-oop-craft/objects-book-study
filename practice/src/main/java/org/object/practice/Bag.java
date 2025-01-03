ddead6fda71a9886d78a4aa81dafcfa15905a79b
권장순 <jsoonworld@gmail.com>
2025-01-03 23:54:22 +0900

    refactor(TicketSeller) : 객체에게 자율성을 부여하기 위한 개선

--- practice/src/main/java/org/object/practice/Bag.java
+++ practice/src/main/java/org/object/practice/Bag.java
@@ -14,24 +14,28 @@ public class Bag {
         this.amount = amount;
     }
 
-    public boolean hasInvitation() {
-        return invitation != null;
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 
-    public boolean hasTicket() {
-        return this.ticket != null;
-    }
-
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    private void minusAmount(Long amount) {
         this.amount -= amount;
     }
 
-    public void plusAmount(Long amount) {
-        this.amount += amount;
-    }
 
 }
